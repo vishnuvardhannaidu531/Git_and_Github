d270d578159e5dd0e9455b37149c2b67d9f236ac
Vishnu <vishnuvardhannaidu531@gmail.com>
2026-02-13 06:09:07 +0530

    bugfixed

---
+++ first.js
@@ -0,0 +1,17 @@
+const button="Added a button";
+console.log(button);
+
+const login="Login page is Added"
+console.log(login);
+
+const footer="Footeyr page is added"
+console.log(footer);
+const payment="Integrated with the paymentgateway"
+console.log(payment);
+
+const upi="Integrated the UPI"
+console.log(upi);
+
+
+const bugfix="Bug is fixed"
+console.log(bugfix);
